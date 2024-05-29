eecd2802914f9c06aa04b28b3e9db213e31ef3e6
Murat Guler <muratguelrofficial@gmail.com>
2024-05-29 22:54:43 +0300

    profile image is updated.

--- src/components/AvatarProfile.jsx
+++ src/components/AvatarProfile.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { auth, db } from "../firebase/firebase";
+import { doc, onSnapshot } from "firebase/firestore";
 
-const AvatarProfile = ({ url = "/default-avatar.svg", bigClass }) => {
+const AvatarProfile = ({ url, bigClass }) => {
+  const defaultImage = "/default-avatar.svg";
+  const [userDetails, setUserDetails] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const docRef = doc(db, "Users", user.uid);
+        const unsubscribeSnapshot = onSnapshot(docRef, (doc) => {
+          if (doc.exists()) {
+            setUserDetails(doc.data());
+          } else {
+          }
+        });
+
+        return () => unsubscribeSnapshot();
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div>
       <img
@@ -13,7 +36,7 @@ const AvatarProfile = ({ url = "/default-avatar.svg", bigClass }) => {
             ? "w-38 h-38 rounded ml-3 border-2 border-blue-900 bg-white"
             : "w-12 h-12 rounded-full cursor-pointer ml-3 border-2 border-blue-900 bg-white hover:border-white transition-all scale-110"
         }
-        src={url}
+        src={userDetails.profilePicture}
       />
     </div>
   );
