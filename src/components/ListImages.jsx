deec689573faafb854f79044197eb896450fb45f
Murat Guler <muratguelrofficial@gmail.com>
2024-05-30 09:04:22 +0300

    now uploaded images can be shown in profile and can be selected as a profile picture.

--- src/components/ListImages.jsx
+++ src/components/ListImages.jsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  listAll,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
 import { auth, db } from "../firebase/firebase";
-import { doc, onSnapshot } from "firebase/firestore";
+import { getFirestore, doc, onSnapshot, setDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
+import { TiDelete } from "react-icons/ti";
 
 const ListImages = () => {
   const [images, setImages] = useState([]);
@@ -49,16 +58,60 @@ const ListImages = () => {
     }
   }, [userDetails]);
 
+  const handleClick = async (url) => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (userDetails.profilePicture === url) {
+      toast.error("Profile picture is the same!");
+    } else {
+      if (user) {
+        const db = getFirestore();
+        const docRef = doc(db, "Users", user.uid);
+        await setDoc(docRef, { profilePicture: url }, { merge: true });
+        toast.success("Profile picture updated successfully!");
+      } else {
+        toast.error("Please login first!");
+      }
+    }
+  };
+
+  const handleDelete = async (url) => {
+    const storage = getStorage();
+    const imageRef = ref(storage, url);
+
+    deleteObject(imageRef)
+      .then(() => {
+        setImages((prevImages) => prevImages.filter((image) => image !== url));
+        toast.success("Image deleted successfully!");
+      })
+      .catch((error) => {
+        toast.error(error);
+      });
+  };
+
   return (
-    <div className="flex gap-5">
-      {images.map((url, index) => (
-        <img
-          key={index}
-          src={url}
-          alt={`User ${index + 1} image`}
-          className="h-14 cursor-pointer hover:ring"
-        />
-      ))}
+    <div className="flex gap-5 flex-wrap">
+      {userDetails ? (
+        <>
+          {images.map((url, index) => (
+            <div key={index} className="relative">
+              <img
+                className="h-16 cursor-pointer hover:ring"
+                src={url}
+                alt={`User ${index + 1} image`}
+                onClick={() => handleClick(url)}
+              />
+              <TiDelete
+                className="absolute top-0 right-0 cursor-pointer mr-1 mt-1 scale-150 hover:text-red-700 transition-all"
+                onClick={() => handleDelete(url)}
+              />
+            </div>
+          ))}
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
