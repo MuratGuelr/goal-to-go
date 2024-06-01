dae0c69130003bc3209c53bf53a0d34a933e18b4
Murat Guler <muratguelrofficial@gmail.com>
2024-06-01 21:49:13 +0300

    now has tools and functionality.

--- src/pages/CreateGoal.jsx
+++ src/pages/CreateGoal.jsx
@@ -1,37 +1,137 @@
-import React from "react";
-import BottomTime from "../components/BottomTime";
+import React, { useState } from "react";
+import CountdownTimer from "../components/CountdownTimer";
+import MiniCountdownTimer from "../components/MiniCountdownTimer";
+import { toast } from "react-toastify";
+import { motion, AnimatePresence } from "framer-motion";
+import Stopwatch from "../components/StopWatch";
+import MiniStopWatch from "../components/MiniStopWatch";
 
 const CreateGoal = () => {
+  const [countdownTimers, setCountdownTimers] = useState([]);
+  const [stopWatchTimers, setStopWatchTimers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleAddCountdownTimer = () => {
+    if (countdownTimers.length > 9) {
+      toast.error("You can't add more than 10!");
+    } else {
+      setCountdownTimers([...countdownTimers, { id: Date.now() }]);
+    }
+  };
+
+  const handleAddStopWatchTimer = () => {
+    if (stopWatchTimers.length > 9) {
+      toast.error("You can't add more than 10!");
+    } else {
+      setStopWatchTimers([...stopWatchTimers, { id: Date.now() }]);
+    }
+  };
+
+  const handleRemoveCountdownTimer = (id) => {
+    setCountdownTimers(countdownTimers.filter((timer) => timer.id !== id));
+  };
+
+  const handleRemoveStopWatchTimer = (id) => {
+    setStopWatchTimers(stopWatchTimers.filter((timer) => timer.id !== id));
+  };
+
+  const handleModalToggle = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <>
-      <BottomTime />
-      <div className="bg-white dark:bg-gray-900 antialiased justify-center h-screen">
-        <form className="max-w-sm mx-auto bg-gray-800 p-5 justify-center w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded">
-          <h1 className="mb-4 text-3xl font-bold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-              Goal To Go
-            </span>
-            <hr className="h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
-          </h1>
-          <div className="flex items-center">
-            <div className="relative ml-5">
-              <input
-                type="text"
-                id="floating_filled"
-                className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-72 text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-              />
-              <label
-                htmlFor="floating_filled"
-                className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
-              >
-                Enter your goal :
-              </label>
-            </div>
-          </div>
-        </form>
+    <div className="dark:bg-gray-900 antialiased h-screen w-full">
+      {countdownTimers.map((timer, index) => (
+        <React.Fragment key={timer.id}>
+          <CountdownTimer
+            tabTotal={index}
+            onRemove={() => handleRemoveCountdownTimer(timer.id)}
+          />
+        </React.Fragment>
+      ))}
+
+      {stopWatchTimers.map((timer, index) => (
+        <React.Fragment key={timer.id}>
+          <Stopwatch
+            tabTotal={index}
+            onRemove={() => handleRemoveStopWatchTimer(timer.id)}
+          />
+        </React.Fragment>
+      ))}
+
+      <div className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 fixed bg-gray-700 cursor-pointer w-1/3 h-1/3 rounded-full flex">
+        <div className="m-auto">
+          <button
+            data-modal-target="popup-modal"
+            data-modal-toggle="popup-modal"
+            type="button"
+            onClick={handleModalToggle}
+          >
+            <img
+              src="/logo-w.png"
+              className="w-72 h-72 rounded-full bg-gray-500 p-8"
+            />
+          </button>
+        </div>
       </div>
-    </>
+
+      <AnimatePresence>
+        {isModalOpen && (
+          <motion.div
+            id="popup-modal"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -50 }}
+            transition={{ duration: 0.3 }}
+            className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0"
+            tabIndex="-1"
+          >
+            <div className="text-center">
+              <div className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute bg-gray-800 cursor-pointer p-5 rounded-lg flex w-2/4">
+                <div className="relative w-full">
+                  <button
+                    className="absolute top-0 right-0 m-4 text-gray-400 hover:text-gray-100"
+                    onClick={handleModalToggle}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="2"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                  <span className="text-7xl font-semibold text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+                    Tools
+                  </span>
+                  <div className="flex flex-wrap justify-center align-middle mt-4 ">
+                    <div
+                      onClick={handleAddCountdownTimer}
+                      className="transition transform hover:scale-110"
+                    >
+                      <MiniCountdownTimer />
+                    </div>
+                    <div
+                      onClick={handleAddStopWatchTimer}
+                      className="transition transform hover:scale-110"
+                    >
+                      <MiniStopWatch />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
 
